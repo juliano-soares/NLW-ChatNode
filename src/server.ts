b77fe4275156406e2acdfe2ab7617a6e55df7159
juliano leonardo soares <julianoleonardosoares@hotmail.com>
2021-04-24 17:56:23 -0300

    Finalizada a segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 
+import "./database";
+import { routes } from './routes';
+
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Olá NLW 05" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "usuario salvo" })
-})
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
 
